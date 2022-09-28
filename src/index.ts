5f72dffee35bac9a6d7ee3489da191da37af17c9
rossana <rossana@recife.pe.gov.br>
2022-09-27 21:00:43 -0300

    Versão 0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import { router } from "./routes"
 
+const port = "variaveldeambiente" || 3000
 
 const server = express()
 server.use(express.json())
 server.use(router)
 
-server.listen(3000, ()=>{
+server.listen(port, ()=>{
     console.log("Rodando porta 3000")
 })
